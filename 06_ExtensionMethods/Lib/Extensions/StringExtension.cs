01d5e3cfc797581ab100f66f9ff308eb8805a07c
Jose Carlos de Moura e Silva <jose.silva@cedrotech.com>
2019-03-26 16:58:35 -0300

    Create project of extensions to String Class.

---
+++ 06_ExtensionMethods/Lib/Extensions/StringExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class StringExtension
+    {
+        public static string FirstLetterToUpper(this String str)
+        {
+            string firstLetter = str.Substring(0, 1);
+
+            return firstLetter.ToUpper() + str.Substring(1);
+        }
+
+        public static string FirstLetterOfNameToUpper(this String str)
+        {
+            string[] listNames = str.Split(" ");
+
+            string newName = string.Empty;
+
+            foreach (var item in listNames)
+            {
+                newName += item.FirstLetterToUpper() + " ";
+            }
+
+            return newName;
+        }
+    }
+}
---
+++ 06_ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _06_ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "josé carlos de moura e silva";
+
+            Console.WriteLine(name.FirstLetterOfNameToUpper().Trim());
+
+            Console.ReadKey();
+        }
+    }
+}
