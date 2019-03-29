10055424e2d73571db99bede25eefc7ddeb06600
Jose Carlos de Moura e Silva <jose.silva@cedrotech.com>
2019-03-29 14:44:29 -0300

    First project to work with Threads.

---
+++ 13_Threads/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace _13_Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadOne = new Thread(ShowNumbers);
+            threadOne.Start();
+
+            for (int i = 0; i <= 1000; i++)
+            {
+                Console.WriteLine($"Number out thread - {i}");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void ShowNumbers()
+        {
+            for (int i = 0; i <= 1000; i++)
+            {
+                Console.WriteLine($"Number - {i}");
+            }
+        }
+    }
+}
